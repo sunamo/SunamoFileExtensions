2c0cc00a8c11748d2db433349b5f751a24aa1105
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-19 14:30:41 +0200

    Removed empty from _sunamo

--- _sunamo/RH.cs
+++
@@ -1,8 +0,0 @@
-namespace SunamoFileExtensions;
-
-//namespace SunamoFileExtensions._sunamo;
-
-//internal class RH
-//{
-//    //internal static Func<Type, GetMemberArgs, List<FieldInfo>> GetConsts;
-//}

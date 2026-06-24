a962a190ccc983ccb935620ba48678698b12a1a4
smutekutek <smutekutek@gmail.com>
2026-06-24 23:03:33 +0200

    Ahoj! Jak ti mohu pomoci?

--- SunamoFileExtensions/AllExtensionsMethods.cs
+++ SunamoFileExtensions/AllExtensionsMethods.cs
@@ -1,14 +1,7 @@
 namespace SunamoFileExtensions;
 
-/// <summary>
-/// Provides methods for working with AllExtensions constants
-/// </summary>
 public class AllExtensionsMethods
 {
-    /// <summary>
-    /// Gets all constant fields from the AllExtensions class
-    /// </summary>
-    /// <returns>List of FieldInfo objects representing extension constants</returns>
     public static List<FieldInfo> GetConsts()
     {
         return typeof(AllExtensions).GetFields().Where(field => field.IsStatic && field.IsLiteral).ToList();
--- SunamoFileExtensions/Args/GetExtensionArgsFileExtensions.cs
+++ SunamoFileExtensions/Args/GetExtensionArgsFileExtensions.cs
@@ -1,17 +1,7 @@
 namespace SunamoFileExtensions.Args;
 
-/// <summary>
-/// Arguments for getting file extensions
-/// </summary>
 public class GetExtensionArgsFileExtensions
 {
-    /// <summary>
-    /// If true, returns the extension in its original case; otherwise, converts to lowercase
-    /// </summary>
     public bool ReturnOriginalCase { get; set; } = false;
-
-    /// <summary>
-    /// If true, files without extensions are returned as-is; otherwise, processed differently
-    /// </summary>
     public bool FilesWithoutExtensionReturnAsIs { get; set; } = false;
 }
